935527c0796d6345727aa05ea5ea9927df986c40
Sergey Shvchenko <stokth.geek@gmail.com>
2024-12-14 19:17:07 +0300

    Changes to be committed: 	new file:   go.mod 	new file:   go.sum 	modified:   main.go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/products", HelloHandler).Methods("GET")
+	http.ListenAndServe(":8080", router)
 }
