7c4619379113b4a88a45841cc82d36f1d9c35c5e
Sergey Shvchenko <stokth.geek@gmail.com>
2024-12-15 15:03:45 +0300

    Changes to be committed: 	new file:   db.go 	modified:   go.mod 	modified:   go.sum 	modified:   main.go 	new file:   orm.go

---
+++ db.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	log.Println("Initializing database...")
+
+	dsn := "host=localhost user=postgres password=root dbname=postgres port=5432 sslmode=disable"
+	var err error
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+}
--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Task struct {
-	Text string `json:"text"`
-}
-
-var tasks = make(map[int]Task)
-
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		fmt.Fprintf(w, "Hello, %s", tasks[0].Text)
+		var messages []Message
+
+		if err := DB.Find(&messages).Error; err != nil {
+			fmt.Fprintln(w, "Ошибка при получении сообщений")
+		}
+
+		json.NewEncoder(w).Encode(&messages)
 	} else {
 		fmt.Fprintln(w, "Поддерживается только GET-запрос")
 	}
@@ -24,18 +24,29 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		var task Task
-		json.NewDecoder(r.Body).Decode(&task)
-		tasks[0] = task
+		var message Message
+
+		json.NewDecoder(r.Body).Decode(&message)
+
+		// Сохраняем сообщение в базе данных
+		if err := DB.Create(&message).Error; err != nil {
+			fmt.Fprintln(w, "Ошибка при сохранении сообщения")
+		}
 	} else {
 		fmt.Fprintln(w, "Поддерживается только POST-запрос")
 	}
 }
 
 func main() {
+	// Вызываем инициализацию базы данных
+	InitDB()
+
+	// Автоматическая миграция модели Message
+	DB.AutoMigrate(&Message{})
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/get", GetHandler).Methods("GET")
-	router.HandleFunc("/api/post", PostHandler).Methods("POST")
+	router.HandleFunc("/api/posts", PostHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
